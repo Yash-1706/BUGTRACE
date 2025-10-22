a7828fdb4c7302a802d23d57b0c908c95131a076
Yash-1706 <gaikwad17yash@gmail.com>
2025-10-23 00:18:49 +0530

    Initial frontend setup with React and Tailwind

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
