b93f667e8cf700bc7bd93e7aec33b09c3fe96bca
Yash-1706 <gaikwad17yash@gmail.com>
2025-10-23 00:17:52 +0530

    Complete backend setup with middleware, utils, and config

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/errorMiddleware');
 
 dotenv.config();
 
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/projects', require('./routes/projectRoutes'));
 app.use('/api/issues', require('./routes/issueRoutes'));
+
+// Error Handler (should be last)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
